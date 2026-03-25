c34fb36b529642edd58dee97128c4046118a12da
Assem Bahnasy <assem.bahnasy@qp.sa>
2026-03-25 14:31:06 +0300

    refractor: removing the excluded and included pests child tables

--- pestcontrol/pest_control_setup/doctype/excluded_pests/excluded_pests.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, QP and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class ExcludedPests(Document):
-	pass
